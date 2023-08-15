d96041f7975dc6bb1d7d1086675f88d55cff6a21
Ubuntu <ubuntu@ip-172-31-54-99.us-west-2.compute.internal>
2023-08-15 20:49:40 +0000

    initial commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,53 @@
 import requests
-import streamlit as st
 import os
+import json
+from dotenv import load_dotenv
+
+# Load variables from .env file
+load_dotenv()
+step_endpoint_url = os.environ.get('STEP_ENDPOINT_URL')
+step_arn = os.environ.get('STEP_ARN')
 step_key = os.environ.get('STEP_API_KEY')
+
+# load in streamlit
+import streamlit as st
 st.set_page_config(page_title="BoM Verse Lookup")
 st.title('BoM Verse Lookup')
 
 # step request function
 def step_request(input_query):
-  endpoint_url = 'https://p1oiumm9wc.execute-api.us-west-2.amazonaws.com/dev'
   headers = {
     'x-api-key': step_key,
     'Content-Type': 'application/json'  # Adjust the content type if needed
   }
 
   # formulate payload based on input query
-  data =  {
-      "input": f'{"query": {input_query}}',
+  data = {
+      "input": f'{{"query": "{input_query}"}}',
       "name": "MyExecution",
-      "stateMachineArn": "arn:aws:states:us-west-2:453900232671:stateMachine:BoM_QA"
-    }
-
-  print(f'Payload: {data}')
-  print(f'Headers: {headers}')
-  
+      "stateMachineArn": step_arn
+  }
+    
   # make request
-  response = requests.post(endpoint_url, json=data, headers=headers)
+  response = requests.post(step_endpoint_url, json=data, headers=headers, timeout=900)
 
   # Check the response
   if response.status_code == 200:
-      st.write(response)
+      # unpack response and return
+      try:
+          step_response = response.json()
+          output = json.loads(step_response['output'])
+          body = json.loads(output['body'])
+          result = body['result']
+          st.write(result)
+      except Exception as e:
+          st.write('Error unpacking response:', e)
   else:
       st.write('Request failed with status code:', response.status_code, 'Response:', response.text)
-
-def generate_response(input_text):
-  response = query_bom(input_text)
-  st.write(response)
 
 with st.form('my_form'):
   text = st.text_area('Enter text:', 'Tell me the full verse that this snippet comes from - "And they came down and went forth upon the face of the earth"')
   submitted = st.form_submit_button('Submit')
   if submitted:
     with st.spinner('Wait for it...'):
-      generate_response(text)
+      step_request(text)
