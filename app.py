2de9068c86b2924ee1680bc3989510f9ab4d3472
Jeremy Watt <jeremywatt@Jeremys-MacBook-Air.local>
2023-09-05 05:15:28 -0700

    email area added and tested

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+st.header(':mailbox: User feedback')
+
+
+contact_form = """
+<form action="https://formsubmit.co/jermwatt@gmail.com" method="POST">
+    <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="your name" required>
+     <input type="email" name="email" placeholder="your email" required>
+    <textarea name="message" placeholder="Details of your problem"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+# Use Local CSS File
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
+
+st.markdown(contact_form, unsafe_allow_html=True)
