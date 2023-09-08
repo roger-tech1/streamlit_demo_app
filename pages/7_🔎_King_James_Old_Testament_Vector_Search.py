1b8d1d4870cae43523a3c294b1c5d1f15cefb2b2
Jeremy Watt <jeremywatt@Jeremys-MacBook-Air.local>
2023-09-08 12:15:58 -0700

    bom keyword and embedder ready

--- pages/7_🔎_King_James_Old_Testament_Vector_Search.py
+++ pages/7_🔎_King_James_Old_Testament_Vector_Search.py
@@ -23,7 +23,7 @@ state_name = 'KingJamesOTVectorSearch'
 
 # Create the relative path based on the parent directory
 parent_directory = os.path.dirname(os.path.dirname(__file__))
-toc_datapath = str(os.path.join(parent_directory, 'tocs/KingJamesOT.json'))
+toc_datapath = str(os.path.join(parent_directory, 'tocs/KingJamesOsT.json'))
 
 # create multiselect
 book_names, book_chunk_lookup, chunk_book_lookup, selected_options = create_multiselect_container(toc_datapath,            
